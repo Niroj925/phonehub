0ef5620cb0475d35ae13a859f1ec19eca3c9ad16
Niroj Thapa <ngtc0925@gmail.com>
2023-05-16 22:33:31 +0545

    deploy backend in render

--- client/pages/payment/failed.js
+++ client/pages/payment/failed.js
@@ -1,6 +1,13 @@
 import React from 'react'
-
+import { useRouter } from 'next/router'
 function failed() {
+  const router=useRouter();
+  const {q}=router.query
+  if(q==='fu'){
+     setTimeout(() => {
+                  router.push('/');
+                }, 3000)
+  }
   return (
     <div>
       payment unsuccess
