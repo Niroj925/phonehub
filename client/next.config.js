50378272aaf0ef516e19441b67d384ad164ac787
Niroj Thapa <ngtc0925@gmail.com>
2023-05-18 13:41:11 +0545

    add .env file

---
+++ client/next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig
+
+module.exports = {
+  env: {
+    BACKEND_API: 'https://ecommerceback-mklr.onrender.com',
+  },
+};
