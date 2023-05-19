65cb15c61b8f348868f47bbf98400081b69a975d
Niroj Thapa <ngtc0925@gmail.com>
2023-05-19 15:10:58 +0545

    auth file

--- server/auth.js
+++ server/auth.js
@@ -28,8 +28,7 @@ const{GOOGLE_CLIENT_ID,GOOGLE_CLIENT_SECRET}=process.env;
   
   ));
 
-//to read and write from passport session we have to do serialize and deserialize 
-//now we can get some datas of sign in email's 
+
 passport.serializeUser(function(user, done) {
     return done(null, user);
    });
